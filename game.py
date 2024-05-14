6be5cf4739b8a4f90d3425d2332c8ca2c24e45c5
Hyunseok Cho <dant0421@gmail.com>
2024-05-14 15:29:53 +0900

    set up game and assets

--- game.py
+++ game.py
@@ -1,19 +1,25 @@
 import pygame
+import os
+import time
+import random
 
 #setup
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
-clock = pygame.time.Clock()
-running = True
+screen = pygame.display.set_mode((750,750))
+pygame.display.set_caption("Space Invader")
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+#load assets (ships)
+redShip = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+greenShip = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
+blueShip = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
 
-    screen.fill("Purple")
+#load assets (players)
+yellowShip = pygame.image.load(os.path.join("assets","pixe_ship_yellow.png"))
 
-    pygame.display.flip()
-    clock.tick(60)
+#load assests (laser)
+redLazer = pygame.image.load(os.path.join("assets","pixel_lazer_red.png"))
+greenLazer = pygame.image.load(os.path.join("assets","pixel_lazer_green.png"))
+blueLazer = pygame.image.load(os.path.join("assets","pixel_lazer_blue.png"))
 
-pygame.quit()
+#load assets (background)
+background = pygame.image.load(os.path.join("assets","background-black.png"))

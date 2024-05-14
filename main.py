a458434fce5be7e43c2983ea7199840dc3f0629d
Hyunseok Cho <dant0421@gmail.com>
2024-05-14 16:49:43 +0900

    added ship class

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 #setup
 pygame.init()
-screen = pygame.display.set_mode((750,750))
+width = 750
+height = 750
+screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Space Invader")
 
 #load assets (ships)
@@ -22,17 +25,45 @@ greenLaser = pygame.image.load(os.path.join("assets","pixel_laser_green.png"))
 blueLaser = pygame.image.load(os.path.join("assets","pixel_laser_blue.png"))
 
 #load assets (background)
-background = pygame.transform.scale(pygame.image.load(os.path.join("assets","background-black.png")),(750,750))
+background = pygame.transform.scale(pygame.image.load(os.path.join("assets","background-black.png")),(width,height))
+
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.ship_laser = None
+        self.lasers = []
+        self.cooltime = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x,self.y,50,50))
 
 def main():
     running = True
     fps = 60
     level = 1
     lives = 5
+    main_font = pygame.font.SysFont("comicsans",50)
+
+    ship = Ship(300,650)
+
     clock = pygame.time.Clock()
 
+    #refreshing window (per fps)
     def redraw_window():
         screen.blit(background,(0,0))
+
+        #draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+        screen.blit(lives_label, (10,10))
+        screen.blit(level_label, (width-level_label.get_width()-10,10))
+
+        ship.draw(screen)
+
         pygame.display.update()
 
     while running: 

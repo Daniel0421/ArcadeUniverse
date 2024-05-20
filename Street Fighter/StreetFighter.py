f6338c6b8451d331e91b39582185d0b0b8557f85
jhkim160589 <jhkim160589@gmail.com>
2024-05-20 12:30:54 +0900

    d

---
+++ Street Fighter/StreetFighter.py
@@ -0,0 +1,22 @@
+import sys, pygame
+pygame.init()
+
+Screen_w = 1000
+Screen_h = 600
+
+screen = pygame.display.set_mode((Screen_w, Screen_h))
+pygame.display.set_caption("Street Fighter")
+
+bg = pygame.image.load("")
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            sys.exit()
+
+
+
+pygame.quit()
